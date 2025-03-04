c7b76ef04381cd8eb968bc21efa4d1ce6146c845
vladimirantipenko <1989sakir@gmail.com>
2025-03-03 18:41:11 -0800

    first action

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
-from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver 
 import pytest
 
 
 @pytest.fixture()
 def browser():
-    chrome_browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    chrome_browser = webdriver.Chrome(options=options)
     chrome_browser.implicitly_wait(10)
     return chrome_browser
 
