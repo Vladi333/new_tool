1e4da3d092c114d9712f197b0c4d38c97395afc9
vladimirantipenko <1989sakir@gmail.com>
2025-04-23 08:40:55 -0700

    add Firefox

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,24 @@
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver 
 import pytest
-import time
-from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
+@pytest.fixture(params=["chrome", "firefox"])
+def browser(request):
+    if request.param == "chrome":
+        options = ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(options=options)
+    elif request.param == "firefox":
+        options = FirefoxOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Firefox(options=options)
+    else:
+        raise ValueError(f"Unsupported browser: {request.param}")
 
-@pytest.fixture()
-def browser():
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    chrome_browser = webdriver.Chrome(options=options)
-    chrome_browser.maximize_window()
-    chrome_browser.implicitly_wait(10)
-    return chrome_browser
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
