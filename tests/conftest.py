91745b2e1a69699a7f940dcccf26b9664c59a9c1
vladimirantipenko <1989sakir@gmail.com>
2025-04-24 18:48:55 -0700

    firefox_add_ci

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,14 +7,16 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 def browser(request):
     if request.param == "chrome":
         options = ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         driver = webdriver.Chrome(options=options)
+
     elif request.param == "firefox":
         options = FirefoxOptions()
-        options.add_argument('--headless')
+        options.add_argument("--headless")
         driver = webdriver.Firefox(options=options)
+
     else:
         raise ValueError(f"Unsupported browser: {request.param}")
 
@@ -22,5 +24,7 @@ def browser(request):
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
+
+
 
 
