41ddfa8a1d88faaae12882de1a9d273e2c6f11cb
vladimirantipenko <1989sakir@gmail.com>
2025-04-24 18:54:34 -0700

    cicd

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,27 +3,30 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-@pytest.fixture(params=["chrome", "firefox"])
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+
+@pytest.fixture()
 def browser(request):
-    if request.param == "chrome":
+    browser_type = request.config.getoption("--browser")
+    if browser_type == "chrome":
         options = ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         driver = webdriver.Chrome(options=options)
-
-    elif request.param == "firefox":
+    elif browser_type == "firefox":
         options = FirefoxOptions()
         options.add_argument("--headless")
         driver = webdriver.Firefox(options=options)
-
     else:
-        raise ValueError(f"Unsupported browser: {request.param}")
+        raise ValueError(f"Unsupported browser: {browser_type}")
 
     driver.maximize_window()
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
+
 
 
 
